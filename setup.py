6ff3cb20834744382b6b76edca24330f51fa1c4a
Jeroen van Straten <jeroen.van.straten@gmail.com>
2019-05-10 15:43:15 +0200

    Add framework for Python tests and code coverage

--- setup.py
+++ setup.py
@@ -1,11 +1,25 @@
 import os
 from setuptools import setup
+from setuptools.command.test import test as TestCommand
 
 def read(fname):
     with open(os.path.join(os.path.dirname(__file__), fname)) as f:
         return f.read()
 
+class NoseTestCommand(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        # Run nose ensuring that argv simulates running nosetests directly
+        import nose
+        nose.run_exit(argv=['nosetests'])
+
 setup(
+
+    # Metadata
     name = "vhdmmio",
     version = "0.0.1",
     author = "Jeroen van Straten",
@@ -17,12 +31,16 @@ setup(
     license = "Apache 2.0",
     keywords = "vhdl mmio registers generator",
     url = "http://packages.python.org/vhdmmio",
-    packages=['vhdmmio'],
-    long_description=read('README.md'),
-    long_description_content_type='text/markdown',
-    classifiers=[
+    long_description = read('README.md'),
+    long_description_content_type = 'text/markdown',
+    classifiers = [
         "Development Status :: 1 - Planning",
         "Topic :: Software Development :: Code Generators",
         "License :: OSI Approved :: Apache Software License",
     ],
+    packages = ['vhdmmio'],
+
+    # Testing
+    tests_require = ['nose', 'coverage'],
+    cmdclass = {'test': NoseTestCommand},
 )
---
+++ vhdmmio/tests/test.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+import vhdmmio
+
+class Test(TestCase):
+    def test_succeeds(self):
+        self.assertTrue(True)
+
+    def test_fails(self):
+        self.assertTrue(False)
