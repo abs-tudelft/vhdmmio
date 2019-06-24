3854d22fd46d0a92bb657c67898dffb0f173279c
Jeroen van Straten <jeroen.van.straten@gmail.com>
2019-06-24 15:33:22 +0200

    Added VHDL type abstraction layer

---
+++ vhdmmio/tests/test_vhdl_types.py
@@ -0,0 +1,171 @@
+from unittest import TestCase
+import os
+import tempfile
+from collections import OrderedDict
+
+import vhdmmio.vhdl.types as types
+
+class TestVhdlTypes(TestCase):
+
+    def test_std_logic(self):
+        self.assertEqual(types.std_logic.name, 'std_logic')
+        self.assertEqual(str(types.std_logic), 'std_logic')
+        self.assertEqual(len(types.std_logic), 1)
+        self.assertEqual(types.std_logic.get_defs(), [])
+        self.assertEqual(list(types.std_logic.gather_types()), [])
+        self.assertEqual(types.std_logic.default, "'0'")
+
+    def test_std_logic_vector(self):
+        self.assertEqual(types.std_logic_vector.name, 'std_logic_vector')
+        self.assertEqual(str(types.std_logic_vector), 'std_logic_vector')
+        with self.assertRaises(ValueError):
+            len(types.std_logic_vector)
+        self.assertEqual(types.std_logic_vector.get_defs(), [])
+        self.assertEqual(list(types.std_logic_vector.gather_types()), [])
+        self.assertEqual(types.std_logic_vector.default, "(others => '0')")
+        self.assertEqual(types.std_logic_vector.get_range(8), '7 downto 0')
+
+        self.assertEqual(
+            types.std_logic_vector.make_signal('test', '0101')[0],
+            'signal test : std_logic_vector(3 downto 0) := "0101"')
+
+        typ = types.SizedArray('test', types.std_logic_vector, '1111000011001010')
+        self.assertEqual(typ.name, 'test')
+        self.assertEqual(str(typ), 'test_type')
+        self.assertEqual(len(typ), 16)
+        self.assertEqual(typ.get_defs(), [
+            'subtype test_type is std_logic_vector(15 downto 0);'
+        ])
+        self.assertEqual(list(typ.gather_types()), ['test_type'])
+        self.assertEqual(typ.default, '"1111000011001010"')
+
+    def test_std_logic_array(self):
+        array = types.Array('test', types.std_logic)
+        self.assertEqual(array.name, 'test')
+        self.assertEqual(str(array), 'test_array')
+        with self.assertRaises(ValueError):
+            len(array)
+        self.assertEqual(array.get_defs(), [
+            'type test_array is array (natural range <>) of std_logic;'
+        ])
+        self.assertEqual(list(array.gather_types()), ['test_array'])
+        self.assertEqual(array.default, "(others => '0')")
+        self.assertEqual(array.get_range(8), '0 to 7')
+
+    def test_sized_std_logic_array(self):
+        array = types.SizedArray('test', types.std_logic, 8)
+        self.assertEqual(array.name, 'test')
+        self.assertEqual(str(array), 'test_type')
+        self.assertEqual(len(array), 8)
+        self.assertEqual(types.gather_defs(array), [
+            'type test_array is array (natural range <>) of std_logic;',
+            'subtype test_type is test_array(0 to 7);',
+        ])
+        self.assertEqual(list(array.gather_types()), ['test_array', 'test_type'])
+        self.assertEqual(array.default, "(others => '0')")
+
+    def test_memory(self):
+        typ = types.SizedArray('mem_ent', types.StdLogicVector('U'), 8)
+        self.assertEqual(typ.name, 'mem_ent')
+        self.assertEqual(str(typ), 'mem_ent_type')
+        self.assertEqual(len(typ), 8)
+        self.assertEqual(typ.get_defs(), [
+            'subtype mem_ent_type is std_logic_vector(7 downto 0);'
+        ])
+        self.assertEqual(list(typ.gather_types()), ['mem_ent_type'])
+        self.assertEqual(typ.default, "(others => 'U')")
+
+        typ = types.Array('mem', typ)
+        self.assertEqual(typ.name, 'mem')
+        self.assertEqual(str(typ), 'mem_array')
+        with self.assertRaises(ValueError):
+            len(typ)
+        self.assertEqual(typ.get_defs(), [
+            'type mem_array is array (natural range <>) of mem_ent_type;'
+        ])
+        self.assertEqual(list(typ.gather_types()), ['mem_ent_type', 'mem_array'])
+        self.assertEqual(typ.default, "(others => (others => 'U'))")
+        self.assertEqual(typ.get_range(8), '0 to 7')
+
+        self.assertEqual(
+            typ.make_signal('test', ['X"%02X"' % idx for idx in range(4)])[0],
+            'signal test : mem_array(0 to 3) := (0 => X"00",@1 => X"01",@2 => X"02",@3 => X"03")')
+
+        typ = types.SizedArray('mem', typ, ['X"%02X"' % idx for idx in range(4)])
+        self.assertEqual(typ.name, 'mem')
+        self.assertEqual(str(typ), 'mem_type')
+        self.assertEqual(len(typ), 32)
+        self.assertEqual(typ.get_defs(), [
+            'subtype mem_type is mem_array(0 to 3);',
+            'constant MEM_RESET : mem_type := (0 => X"00",@1 => X"01",@2 => X"02",@3 => X"03");',
+        ])
+        self.assertEqual(list(typ.gather_types()), ['mem_ent_type', 'mem_array', 'mem_type'])
+        self.assertEqual(typ.default, 'MEM_RESET')
+
+    def test_record(self):
+        self.maxDiff = None
+        byte_type = types.SizedArray('byte', types.std_logic_vector, 8)
+        byte_array = types.Array('byte', byte_type)
+        record = types.Record('test', ('a', byte_type))
+        with self.assertRaisesRegex(ValueError, 'name conflict'):
+            record.append('a', types.std_logic)
+        record.append('b', types.std_logic)
+        record.append('c', types.std_logic_vector, '0011')
+        record.append('d', types.std_logic, '1')
+        record.append('e', byte_array, ['X"01"', 'X"02"', 'X"03"', 'X"04"'])
+        self.assertEqual(record.name, 'test')
+        self.assertEqual(str(record), 'test_type')
+        self.assertEqual(len(record), 46)
+        self.assertEqual(types.gather_defs(record, byte_type, byte_type), [
+            'subtype byte_type is std_logic_vector(7 downto 0);',
+            'type byte_array is array (natural range <>) of byte_type;',
+            'subtype test_e_type is byte_array(0 to 3);',
+            'constant TEST_E_RESET : test_e_type := (0 => X"01",@1 => X"02",@2 => X"03",@3 => X"04");',
+            'type test_type is record',
+            '  a : byte_type;',
+            '  b : std_logic;',
+            '  c : std_logic_vector(3 downto 0);',
+            '  d : std_logic;',
+            '  e : test_e_type;',
+            'end record;',
+            'constant TEST_RESET : test_type := (',
+            "  a => (others => '0'),",
+            "  b => '0',",
+            '  c => "0011",',
+            '  d => 1,',
+            '  e => TEST_E_RESET',
+            ');',
+        ])
+        byte_type = types.SizedArray('byte', types.std_logic_vector, 7)
+        with self.assertRaisesRegex(ValueError, 'name conflict'):
+            types.gather_defs(record, byte_type, byte_type)
+        self.assertEqual(record.default, 'TEST_RESET')
+
+        self.assertEqual(record.make_input('test')[0], 'test : in test_type := TEST_RESET')
+        self.assertEqual(record.make_output('test')[0], 'test : out test_type := TEST_RESET')
+        self.assertEqual(record.make_signal('test')[0], 'signal test : test_type := TEST_RESET')
+        self.assertEqual(record.make_variable('test')[0], 'variable test : test_type := TEST_RESET')
+        self.assertEqual(record.make_constant('test')[0], 'constant TEST : test_type := TEST_RESET')
+        self.assertEqual(record.make_generic('test')[0], 'TEST : test_type := TEST_RESET')
+        self.assertEqual(record.make_generic('test', 'foo')[0], 'TEST : test_type := foo')
+
+        _, test = record.make_input('test')
+        self.assertEqual(str(test), 'test')
+        self.assertEqual(str(test.typ), 'test_type')
+        self.assertEqual(str(test.a), 'test.a')
+        self.assertEqual(str(test.a.typ), 'byte_type')
+        self.assertEqual(str(test.a[0]), 'test.a(0)')
+        self.assertEqual(str(test.a[0].typ), 'std_logic')
+        self.assertEqual(str(test.a[2, 3]), 'test.a(4 downto 2)')
+        self.assertEqual(str(test.a[2, 3].typ), '<slice of std_logic>')
+        self.assertEqual(test.a[2, 3].typ.count, 3)
+        self.assertEqual(str(test.a[2, 'test']), 'test.a(test + 1 downto 2)')
+        self.assertEqual(test.a[2, 'test'].typ.count, 'test')
+        self.assertEqual(str(test.a[1, 'test']), 'test.a(test downto 1)')
+        self.assertEqual(str(test.a[0, 'test']), 'test.a(test - 1 downto 0)')
+        self.assertEqual(str(test.a[0, 'foo + bar']), 'test.a((foo + bar) - 1 downto 0)')
+        self.assertEqual(str(test.a['test', 3]), 'test.a(test + 2 downto test)')
+        self.assertEqual(str(test.a['foo + bar', 5]), 'test.a((foo + bar) + 4 downto foo + bar)')
+        self.assertEqual(str(test.a['foo', 'bar']), 'test.a(foo + bar - 1 downto foo)')
+        with self.assertRaisesRegex(TypeError, 'not an array'):
+            test.b[0]
---
+++ vhdmmio/vhdl/types.py
@@ -0,0 +1,471 @@
+"""Module with abstractions for VHDL types."""
+
+import re
+from collections import OrderedDict
+
+def _count_and_offset_to_high_and_low(count, offset):
+    low = str(offset)
+    if isinstance(count, str) and not re.match('[a-zA-Z0-9_]+$', count):
+        count = '(%s)' % count
+    if isinstance(offset, str) and not re.match('[a-zA-Z0-9_]+$', offset):
+        offset = '(%s)' % offset
+    def static_offset(expression, offset):
+        if offset == 0:
+            return expression
+        if offset < 0:
+            return '%s - %d' % (expression, -offset)
+        return '%s + %d' % (expression, offset)
+    if isinstance(count, str) and isinstance(offset, str):
+        high = '%s + %s - 1' % (offset, count)
+    elif isinstance(count, str):
+        high = static_offset(count, offset - 1)
+    elif isinstance(offset, str):
+        high = static_offset(offset, count - 1)
+    else:
+        high = offset + count - 1
+    return high, low
+
+
+class _Base():
+    """Base class for abstracting VHDL types.
+
+    Before a non-primitive type can be used in VHDL, it must be defined. You
+    can fetch the required definitions using `get_defs()` (for one complex
+    type) or `gather_defs()` (for multiple complex types).
+
+    Unlike VHDL, these types carry a default value. For complex types, these
+    default values are written to VHDL as constants. The idea is that any
+    complex signal or variable has a sane default/reset value associated with
+    it, versus just the default 'U' and so on."""
+
+    def __init__(self, name, default):
+        """Constructs the base class; don't call this directly."""
+        self._name = name
+        self._default = default
+
+    @property
+    def name(self):
+        """Returns the name of this type."""
+        return self._name
+
+    @property
+    def default(self):
+        """Returns the default value for this type."""
+        return self._default
+
+    @property
+    def incomplete(self):
+        """Indicates whether this is an incomplete array type."""
+        return False
+
+    @property
+    def primitive(self):
+        """Indicates whether this is a primitive type."""
+        return False
+
+    @staticmethod
+    def __len__():
+        """Returns the number of bits in this type."""
+        raise NotImplementedError('base class for VHDL types does not have a bit count')
+
+    def __str__(self):
+        """Returns the name of this type, including suffix."""
+        if self.primitive:
+            return self.name
+        return self.name + '_type'
+
+    @staticmethod
+    def get_defs():
+        """Returns the type definition(s) for this type as a list of strings
+        representing lines of code."""
+        return []
+
+    def gather_types(self, into=None):
+        """Gather all the types needed by this type into an ordered dict from
+        name to type."""
+        if into is None:
+            into = OrderedDict()
+        if not self.primitive:
+            prev = into.get(str(self), None)
+            if prev is None:
+                into[str(self)] = self
+            elif prev is not self:
+                raise ValueError('type name conflict: %s' % self)
+        return into
+
+    def _instantiate(self, fmt, name, arg):
+        typ = str(self)
+        if self.incomplete:
+            anonymous_subtype = SizedArray(None, self, arg)
+            typ += '(%s)' % self.get_range(anonymous_subtype.count) #pylint: disable=E1101
+            default = anonymous_subtype.default
+        elif arg is not None:
+            default = arg
+        else:
+            default = self.default
+        return fmt.format(name=name, default=str(default), typ=typ), _Object(name, self)
+
+    def make_input(self, name, arg=None):
+        """Makes an input signal from this type. Returns a two-tuple of the
+        signal declaration string excluding semicolon and the instantiated
+        object."""
+        return self._instantiate('{name} : in {typ} := {default}', name, arg)
+
+    def make_output(self, name, arg=None):
+        """Makes an output signal from this type. Returns a two-tuple of the
+        signal declaration string excluding semicolon and the instantiated
+        object."""
+        return self._instantiate('{name} : out {typ} := {default}', name, arg)
+
+    def make_signal(self, name, arg=None):
+        """Makes an internal signal from this type. Returns a two-tuple of the
+        signal declaration string excluding semicolon and the instantiated
+        object."""
+        return self._instantiate('signal {name} : {typ} := {default}', name, arg)
+
+    def make_variable(self, name, arg=None):
+        """Makes a variable from this type. Returns a two-tuple of the
+        signal declaration string excluding semicolon and the instantiated
+        object."""
+        return self._instantiate('variable {name} : {typ} := {default}', name, arg)
+
+    def make_constant(self, name, arg=None):
+        """Makes a constant from this type. Returns a two-tuple of the
+        signal declaration string excluding semicolon and the instantiated
+        object."""
+        return self._instantiate('constant {name} : {typ} := {default}', name.upper(), arg)
+
+    def make_generic(self, name, arg=None):
+        """Makes a generic from this type. Returns a two-tuple of the
+        signal declaration string excluding semicolon and the instantiated
+        object."""
+        return self._instantiate('{name} : {typ} := {default}', name.upper(), arg)
+
+
+class StdLogic(_Base):
+    """Representation of the `std_logic` primitive."""
+
+    def __init__(self, default=0):
+        """Constructs an `std_logic` representation with the given default
+        value."""
+        super().__init__('std_logic', "'%s'" % default)
+
+    @staticmethod
+    def __len__():
+        """Returns the number of bits in this type."""
+        return 1
+
+    @property
+    def primitive(self):
+        """Indicates whether this is a primitive type."""
+        return True
+
+
+class Array(_Base):
+    """Representation of an incomplete array type."""
+
+    def __init__(self, name, element_type):
+        """Constructs an incomplete array type from the given element type."""
+        super().__init__(name, '(others => %s)' % element_type.default)
+        self._element_type = element_type
+
+    @property
+    def incomplete(self):
+        """Indicates whether this is an incomplete array type."""
+        return True
+
+    @property
+    def element_type(self):
+        """Returns the underlying element type."""
+        return self._element_type
+
+    def __len__(self):
+        """Returns the number of bits in this type."""
+        raise ValueError('incomplete array does not have a bit count yet')
+
+    def __str__(self):
+        """Returns the name of this type, including suffix."""
+        if self.primitive:
+            return self.name
+        return self.name + '_array'
+
+    def get_defs(self):
+        """Returns the type definition(s) for this type as a list of strings
+        representing lines of code."""
+        return ['type %s is array (natural range <>) of %s;' % (
+            self, self.element_type)]
+
+    @staticmethod
+    def get_range(count, offset=0):
+        """Returns the code for a range of this array type of `count`
+        elements."""
+        high, low = _count_and_offset_to_high_and_low(count, offset)
+        return '%s to %s' % (low, high)
+
+    def gather_types(self, into=None):
+        """Gather all the types needed by this type into an ordered dict from
+        name to type."""
+        into = self.element_type.gather_types(into)
+        return super().gather_types(into)
+
+
+class StdLogicVector(Array):
+    """Representation of the `std_logic_vector` primitive."""
+
+    def __init__(self, bit_default=0):
+        """Constructs an `std_logic` representation with the given default
+        value."""
+        super().__init__('std_logic_vector', StdLogic(bit_default))
+
+    def get_defs(self):
+        """Returns the type definition(s) for this type as a list of strings
+        representing lines of code."""
+        return []
+
+    @staticmethod
+    def get_range(count, offset=0):
+        """Returns the code for a range of this array type of `count`
+        elements."""
+        high, low = _count_and_offset_to_high_and_low(count, offset)
+        return '%s downto %s' % (high, low)
+
+    @property
+    def primitive(self):
+        """Indicates whether this is a primitive type."""
+        return True
+
+
+class SizedArray(_Base):
+    """Representation of a complete (fixed-size) array type."""
+
+    def __init__(self, name, typ, count_or_default):
+        """Completes the given incomplete array type by assigning a size to it,
+        or constructs a sized array with elements of the given type.
+
+        `count_or_default` can take three kinds of values:
+
+         - `int`: the array has the given number of elements, which all default
+           to the element type's default using an `others` statement.
+         - `str`: the array type must be an `std_logic_vector`, which defaults
+           to the given bit string.
+         - an iterable or default values.
+        """
+        if not typ.incomplete:
+            typ = Array(name, typ)
+
+        self._default_def = None
+        if isinstance(count_or_default, int):
+            super().__init__(name, typ.default)
+            self._count = count_or_default
+        elif isinstance(count_or_default, str):
+            super().__init__(name, '"%s"' % count_or_default)
+            self._count = len(count_or_default)
+        else:
+            count_or_default = list(count_or_default)
+            self._count = len(count_or_default)
+            default = '(%s)' % ',@'.join(
+                ('%d => %s' % x for x in enumerate(count_or_default)))
+            if name is None:
+                super().__init__(name, default)
+            else:
+                super().__init__(name, name.upper() + '_RESET')
+                self._default_def = default
+
+        self._array_type = typ
+
+    @property
+    def array_type(self):
+        """Returns the underlying array type."""
+        return self._array_type
+
+    @property
+    def element_type(self):
+        """Returns the underlying element type."""
+        return self._array_type.element_type
+
+    @property
+    def count(self):
+        """Returns the number of array elements in this array subtype."""
+        return self._count
+
+    def get_range(self, *args, **kwargs):
+        """Returns the code for a range of this array type of `count`
+        elements."""
+        return self._array_type.get_range(*args, **kwargs)
+
+    def __len__(self):
+        """Returns the number of bits in this type."""
+        return self._count * len(self.element_type)
+
+    def get_defs(self):
+        """Returns the type definition(s) for this type as a list of strings
+        representing lines of code."""
+        defs = ['subtype %s is %s(%s);' % (
+            self, self.array_type, self.array_type.get_range(self.count))]
+        if self._default_def is not None:
+            defs.append('constant %s : %s := %s;' % (
+                self.default, self, self._default_def))
+        return defs
+
+    def gather_types(self, into=None):
+        """Gather all the types needed by this type into an ordered dict from
+        name to type."""
+        into = self.array_type.gather_types(into)
+        return super().gather_types(into)
+
+
+class Slice:
+    """Representation of an arbitrarily sized array slice."""
+
+    def __init__(self, element_type, count):
+        """Constructs a slice."""
+        super().__init__()
+        self._element_type = element_type
+        self._count = count
+
+    @property
+    def element_type(self):
+        """Returns the underlying element type."""
+        return self._element_type
+
+    @property
+    def count(self):
+        """Returns the number of elements in the slice. This may be a string if
+        the count is a VHDL expression."""
+        return self._count
+
+    def __str__(self):
+        return '<slice of %s>' % self.element_type
+
+
+class Record(_Base):
+    """Representation of a record type."""
+
+    def __init__(self, name, *elements):
+        """Constructs a record type from the given elements. Elements passed
+        to `elements` must be N-tuples, which are just passed to
+        `append()`."""
+        super().__init__(name, name.upper() + '_RESET')
+        self._elements = []
+        self._names = set()
+        for args in elements:
+            self.append(*args)
+
+    def append(self, name, typ, arg=None):
+        """Adds an element to the record.
+
+        If `typ` is an incomplete array, it is auto-completed with the array
+        size, bit string (for `std_logic_vector`s only), or iterable of default
+        values specified by `arg`. Otherwise, `arg` optionally specifies an
+        override for the default value."""
+        if name in self._names:
+            raise ValueError('record entry name conflict: %s' % name)
+        self._names.add(name)
+        count = None
+        if typ.incomplete:
+            expanded = SizedArray('%s_%s' % (self.name, name), typ, arg)
+            default = expanded.default
+            if default.isupper():
+                typ = expanded
+            else:
+                count = expanded.count
+        elif arg is not None:
+            default = arg
+        else:
+            default = typ.default
+        self._elements.append((name, typ, count, default))
+
+    @property
+    def elements(self):
+        """Returns the elements of this record as a dict."""
+        return {name: typ for name, typ, *_ in self._elements}
+
+    def __len__(self):
+        """Returns the number of bits in this type."""
+        return sum((
+            len(typ) if count is None else len(typ.element_type) * count
+            for _, typ, count, _ in self._elements))
+
+    def get_defs(self):
+        """Returns the type definition(s) for this type as a list of strings
+        representing lines of code."""
+        defs = ['type %s is record' % self]
+        for name, typ, count, _ in self._elements:
+            if count is None:
+                defs.append('  %s : %s;' % (name, typ))
+            else:
+                defs.append('  %s : %s(%s);' % (name, typ, typ.get_range(count)))
+        defs.append('end record;')
+        defs.append('constant %s : %s := (' % (self.default, self))
+        for name, _, _, default in self._elements:
+            defs.append('  %s => %s,' % (name, default))
+        defs[-1] = defs[-1][:-1]
+        defs.append(');')
+        return defs
+
+    def gather_types(self, into=None):
+        """Gather all the types needed by this type into an ordered dict from
+        name to type."""
+        if into is None:
+            into = OrderedDict()
+        for _, element_type, _, _ in self._elements:
+            into = element_type.gather_types(into)
+        return super().gather_types(into)
+
+
+class _Object:
+    """Base class for a VHDL object."""
+
+    def __init__(self, name, typ):
+        super().__init__()
+        self._name = name
+        self._typ = typ
+        if isinstance(typ, Record):
+            for element_name, element_type in typ.elements.items():
+                setattr(
+                    self, element_name,
+                    _Object(
+                        '%s.%s' % (name, element_name),
+                        element_type))
+
+    @property
+    def name(self):
+        """Returns the name of this object."""
+        return self._name
+
+    @property
+    def typ(self):
+        """Returns the type of this object."""
+        return self._typ
+
+    def __str__(self):
+        return self._name
+
+    def __getitem__(self, index):
+        if not isinstance(self.typ, (Array, SizedArray)):
+            raise TypeError('%s is not an array' % self)
+        if isinstance(index, tuple):
+            offset, count = index
+            rnge = self.typ.get_range(count, offset)
+            return _Object(
+                '%s(%s)' % (self.name, rnge),
+                Slice(self.typ.element_type, count))
+        return _Object(
+            '%s(%s)' % (self.name, index),
+            self.typ.element_type)
+
+
+def gather_defs(*types):
+    """Returns the type definition(s) for the given types as a list of
+    strings representing lines of code."""
+    all_types = None
+    for typ in types:
+        all_types = typ.gather_types(all_types)
+    defs = []
+    for typ in all_types.values():
+        defs.extend(typ.get_defs())
+    return defs
+
+
+std_logic = StdLogic() #pylint: disable=C0103
+std_logic_vector = StdLogicVector() #pylint: disable=C0103
