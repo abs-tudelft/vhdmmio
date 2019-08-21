d3f50420007088d222419dd733b0e9c8e9995a9e
Jeroen van Straten <jeroen.van.straten@gmail.com>
2019-08-21 16:50:44 +0200

    Minor changes to interrupt logic template

--- vhdmmio/vhdl/behavior/interrupt-read.template.vhd
+++ vhdmmio/vhdl/behavior/interrupt-read.template.vhd
@@ -1,6 +1,6 @@
-@ Read mode: $cfg.bus_read$.
-$r_data$ := $v$;
-$if cfg.bus_read == 'clear'
-$v$ := '0';
-$endif
-r_ack := true;
+|@ Read mode: read $'and clear %s' % cfg.mode if cfg.bus_read == 'clear' else cfg.mode$.
+|$r_data$ := $v$;
+|$if cfg.bus_read == 'clear'
+  |$v$ := '0';
+|$endif
+|r_ack := true;
--- vhdmmio/vhdl/behavior/interrupt-write.template.vhd
+++ vhdmmio/vhdl/behavior/interrupt-write.template.vhd
@@ -1,11 +1,13 @@
-@ Write mode: $cfg.bus_write$.
-$if cfg.bus_write == 'enabled'
-$v$ := ($v$ and not $w_strobe$) or $w_data$;
-$endif
-$if cfg.bus_write == 'clear'
-$v$ := $v$ and not $w_data$;
-$endif
-$if cfg.bus_write == 'set'
-$v$ := $v$ or $w_data$;
-$endif
-w_ack := true;
+|$if cfg.bus_write == 'enabled'
+  |@ Write mode: set $cfg.mode$.
+  |$v$ := ($v$ and not $w_strobe$) or $w_data$;
+|$endif
+|$if cfg.bus_write == 'clear'
+  |@ Write mode: bit-clear $cfg.mode$.
+  |$v$ := $v$ and not $w_data$;
+|$endif
+|$if cfg.bus_write == 'set'
+  |@ Write mode: bit-set $cfg.mode$.
+  |$v$ := $v$ or $w_data$;
+|$endif
+|w_ack := true;
